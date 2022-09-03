8ae871f888d2dd2a78e7f51127fa4f47f1baf6b2
marksenee <mark233891@gmail.com>
2022-09-03 11:15:45 +0900

    [Feat] 로그인 UI 구현

--- daisso/src/Router.js
+++ daisso/src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Post from "./component/post/Post";
+import Login from "./component/signin/SignInModal";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<Post />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ daisso/src/component/signin/styles.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const ContainerStyle = styled.div`
+  width: 600px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  margin: auto;
+  flex-direction: column;
+
+  border-style: solid;
+  border-color: #ddd;
+  border-radius: 10px;
+`;
+
+const LoginContainer = styled.div`
+  width: 300px;
+  height: 200px;
+  margin: 30px 0px 20px 0px;
+`;
+
+const LoginFormField = styled.input`
+  width: 250px;
+  height: 40px;
+  padding-left: 10px;
+  border-style: solid;
+  border-radius: 10px;
+  border-color: #ddd;
+  margin: 10px 20px 5px 20px;
+`;
+
+const ButtonContainer = styled.div`
+  margin: 10px 10px 10px 25px;
+`;
+
+const ButtonElement = styled.button`
+  width: 120px;
+  height: 40px;
+  justify-content: center;
+  background-color: #e0e0e0;
+  border-style: solid;
+  border-radius: 10px;
+  border-color: transparent;
+  margin: 10px 10px 10px 0px;
+
+  &:hover {
+    background-color: #616161;
+    color: white;
+  }
+`;
+
+export {
+  ContainerStyle,
+  LoginContainer,
+  LoginFormField,
+  ButtonContainer,
+  ButtonElement,
+};
