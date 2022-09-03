ba44bcf588c4e7c178c51ab769c93957b658dff4
marksenee <mark233891@gmail.com>
2022-09-04 00:43:19 +0900

    [Add] router 추가

--- daisso/src/Router.js
+++ daisso/src/Router.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Post from "./component/post/Post";
-import Login from "./component/signin/SignInModal";
+import SignIn from "./component/signin/SignIn";
+import PostReview from "./component/review/addPost/PostReview";
+import EditReview from "./component/review/edit/EditReview";
 
 const Router = () => {
   return (
@@ -10,7 +12,11 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<Post />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/post/review" element={<PostReview />} />
+        <Route path="/post/review/:id" element={<PostReview />} />
+        <Route path="/edit/review" element={<EditReview />} />
+        <Route path="/edit/review/:id" element={<EditReview />} />
       </Routes>
     </BrowserRouter>
   );
