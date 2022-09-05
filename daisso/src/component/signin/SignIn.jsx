0f03e20b49400d9409f1aac3628236c6e5617a76
marksenee <mark233891@gmail.com>
2022-09-05 15:27:22 +0900

    [Add] 임시 로그인 기능 구현

--- daisso/src/component/signin/SignIn.jsx
+++ daisso/src/component/signin/SignIn.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SingupModal from "../signup/SignUpModal";
 import useShowModal from "../../hooks/useShowModal";
+import { __getUser } from "../../redux/modules/users";
 import {
   ContainerStyle,
   LoginContainer,
@@ -11,13 +12,29 @@ import {
   LogoButton,
 } from "./styles";
 import { useNavigate } from "react-router-dom";
+import useInput from "../../hooks/useInput";
 
 function SignIn() {
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
   const [isModal, ModalHandler] = useShowModal();
+  const [userId, setUserId, onChange] = useInput();
+  const [password, setPassword, onChangePassword] = useInput();
 
   const { users } = useSelector((state) => state.users);
   console.log("users", users);
+
+  const onClickLogin = (userId, password) => {
+    if (userId === "" || password === "") {
+      alert("아이디와 비밀번호를 입력하세요");
+    } else {
+      dispatch(__getUser({ userId, password }));
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <>
@@ -26,10 +43,23 @@ function SignIn() {
           <img src="../daisso.jpeg" width="75" height="75" />
         </LogoButton>
         <LoginContainer>
-          <LoginFormField placeholder="아이디" />
-          <LoginFormField placeholder="비밀번호" />
+          <LoginFormField
+            placeholder="아이디"
+            title="userId"
+            value={userId}
+            onChange={onChange}
+          />
+          <LoginFormField
+            placeholder="비밀번호"
+            title="password"
+            value={password}
+            type="password"
+            onChange={onChangePassword}
+          />
           <ButtonContainer>
-            <ButtonElement>로그인</ButtonElement>
+            <ButtonElement onClick={() => onClickLogin(userId, password)}>
+              로그인
+            </ButtonElement>
             <ButtonElement onClick={() => ModalHandler()}>
               회원가입
             </ButtonElement>
