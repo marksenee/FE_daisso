9f96dadc1ec25cde29ff98810bd1d9484acbb103
marksenee <mark233891@gmail.com>
2022-09-05 15:48:23 +0900

    [Add] header 부분 로그인 처리

--- daisso/src/component/header/Header.jsx
+++ daisso/src/component/header/Header.jsx
@@ -12,11 +12,14 @@ import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { useNavigate } from "react-router-dom";
 import SingupModal from "../signup/SignUpModal";
 import useShowModal from "../../hooks/useShowModal";
+import useToken from "../../hooks/useToken";
 
 function Header() {
   const navigate = useNavigate();
   const [view, setView] = useState(false);
   const [isModal, ModalHandler] = useShowModal();
+  // token
+  const token = useToken();
 
   return (
     <HeaderDiv>
@@ -25,29 +28,36 @@ function Header() {
       </HeaderLogo>
       <HeaderRight>
         {/* TODO: 로그인 후 조건문 사용 */}
-        {/* <div onClick={() => navigate("/login")}>로그인</div> */}
-        <HeaderButton onClick={() => navigate(`/login`)}>로그인</HeaderButton>
-        <HeaderButton onClick={() => ModalHandler()}>회원가입</HeaderButton>
-        <SingupModal show={isModal} modalHandler={ModalHandler} />
-        <HeaderUl
-          onClick={() => {
-            setView(!view);
-          }}
-        >
-          반가워요, nickname 님!{" "}
-          {view ? (
-            <KeyboardArrowUpIcon
-              viewBox="0 -5 24 24"
-              style={{ marginLeft: "5px" }}
-            />
-          ) : (
-            <KeyboardArrowDownIcon
-              viewBox="0 -5 24 24"
-              style={{ marginLeft: "5px" }}
-            />
-          )}
-          {view && <Dropdown />}
-        </HeaderUl>
+        {!token ? (
+          <>
+            {" "}
+            <HeaderButton onClick={() => navigate(`/login`)}>
+              로그인
+            </HeaderButton>
+            <HeaderButton onClick={() => ModalHandler()}>회원가입</HeaderButton>
+            <SingupModal show={isModal} modalHandler={ModalHandler} />
+          </>
+        ) : (
+          <HeaderUl
+            onClick={() => {
+              setView(!view);
+            }}
+          >
+            반가워요, nickname 님!{" "}
+            {view ? (
+              <KeyboardArrowUpIcon
+                viewBox="0 -5 24 24"
+                style={{ marginLeft: "5px" }}
+              />
+            ) : (
+              <KeyboardArrowDownIcon
+                viewBox="0 -5 24 24"
+                style={{ marginLeft: "5px" }}
+              />
+            )}
+            {view && <Dropdown />}
+          </HeaderUl>
+        )}
       </HeaderRight>
     </HeaderDiv>
   );
