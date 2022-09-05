7888f53b877bc891d503828d0a25f1aa2247b5ae
marksenee <mark233891@gmail.com>
2022-09-05 15:25:53 +0900

    [Add] user 임시 로그인 처리

--- daisso/src/redux/modules/users.js
+++ daisso/src/redux/modules/users.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import generateToken from "../../utils/Token";
+import { setCookie, removeCookie } from "../../utils/Cookie";
 
 // url
 const REACT_APP_API_USERS_URL = `http://localhost:5001/users`;
@@ -13,6 +15,36 @@ const initialState = {
 };
 
 // thunk function
+// [특정 사용자 확인 - 로그인]
+export const __getUser = createAsyncThunk(
+  "getUser",
+  async (payload, thunkAPI) => {
+    const { userId, password } = payload;
+    try {
+      await axios
+        .get(REACT_APP_API_USERS_URL + `?userId=${userId}`)
+        .then((res) => {
+          console.log("res", res.data[0]);
+          if (res.data[0] == undefined) {
+            alert("사용자가 존재하지 않습니다.");
+          } else {
+            if (res.data[0].password === password) {
+              alert("로그인 성공");
+              const token = generateToken(userId); // 임시 토큰
+              setCookie("access_token", token);
+              return thunkAPI.fulfillWithValue(res.data);
+            } else {
+              alert("비밀번호를 다시 확인하세요");
+              return thunkAPI.rejectWithValue("비밀번호를 다시 확인하세요");
+            }
+          }
+        });
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 // [회원가입]
 export const __createUsers = createAsyncThunk(
   "createUsers",
@@ -43,6 +75,9 @@ export const users = createSlice({
       });
     },
     [__createUsers.rejected]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__getUser.rejected]: (state, action) => {
       state.isLoading = false;
     },
   },
