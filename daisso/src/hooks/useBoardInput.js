68b8830c0ec2ff4c0b71743a051422809326752e
marksenee <mark233891@gmail.com>
2022-09-06 02:03:20 +0900

    [Feat] 사진첨부 custom hook

---
+++ daisso/src/hooks/useBoardInput.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const useBoardInput = (initialState) => {
+  console.log("ddd", initialState);
+  const [input, setInput] = useState(initialState);
+
+  const handler = (e) => {
+    const { name, value } = e.target;
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+  return [input, setInput, handler];
+};
+
+export default useBoardInput;
